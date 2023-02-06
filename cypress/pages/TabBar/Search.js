e4db4c055b4f6557ffb219d7fbc91a7dbae01139
clouvisdenosta <clouvis.denosta@norimaconsulting.com>
2023-02-06 10:33:07 +0800

    imported examples json and added verifyAccount

--- cypress/pages/TabBar/Search.js
+++ cypress/pages/TabBar/Search.js
@@ -1,4 +1,4 @@
-import testdata from "../../utils/testdata";
+import examples from "../../fixtures/example.json";
 import { TabBar } from "./TabBar";
 
 const tabBar = new TabBar();
@@ -9,14 +9,20 @@ export class Search {
         accountSearchTabMenu:() => cy.get('#TabBar-SearchTab-Search_AccountSearch'),
         accountSearchAccountNumber:() => cy.get('#AccountSearch-AccountSearchScreen-AccountSearchDV-AccountNumber'),
         accountSearchResults:() => cy.get('#AccountSearch-AccountSearchScreen-AccountSearchResultsLV'),
-        searchMenuLinksSearch_AccountSearch:() => cy.get('#Search-MenuLinks-Search_AccountSearch')
+        searchMenuLinksSearch_AccountSearch:() => cy.get('#Search-MenuLinks-Search_AccountSearch'),
+        accountSearchSearchButton:() => cy.get('#AccountSearch-AccountSearchScreen-AccountSearchDV-SearchAndResetInputSet-SearchLinksInputSet-Search')
     }
 
     searchByAccount(){
         tabBar.elements.moreOptions().click();
         this.elements.searchTab().click();
         this.elements.searchMenuLinksSearch_AccountSearch().click();
-        this.elements.accountSearchAccountNumber().type(testdata.accountNumber);
-        // expect(this.elements.accountSearchResults().find('tr')).gt(1);
+        this.elements.accountSearchAccountNumber().type(examples.accountNumber);
+        this.elements.accountSearchSearchButton().click();
+    }
+
+    verifyAccount(){
+        this.elements.accountSearchResults().find('tr').its('length').should('be.gt' , 1);
+        // expect(this.elements.accountSearchResults().find('tr').its('length')).to.be.gt(1);
     }
 }
