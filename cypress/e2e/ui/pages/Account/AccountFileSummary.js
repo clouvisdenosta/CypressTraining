10dc8f593415379902e254f76d7d1034704bcbbd
clouvis <denostaclouvis@gmail.com>
2023-06-29 07:35:57 +0800

    added the account holder element

--- cypress/e2e/ui/pages/Account/AccountFileSummary.js
+++ cypress/e2e/ui/pages/Account/AccountFileSummary.js
@@ -1,6 +1,7 @@
 export class AccountFileSummary {
     elements = {
         accountNumber:() => cy.get('#AccountFile_Summary-AccountSummaryDashboard-AccountDetailsDetailViewTile-AccountDetailsDetailViewTile_DV-AccountNumber'),
+        accountHolder:() => cy.get('#AccountFile_Summary-AccountSummaryDashboard-AccountDetailsDetailViewTile-AccountDetailsDetailViewTile_DV-AccountHolder'),
         accountDetails:() => cy.get('#AccountFile_Summary-AccountSummaryDashboard-AccountDetailsDetailViewTile')
     }
 }
