a8bc9e6df0103874a9a2d41ce3693ed04ebc8f30
clouvis <denostaclouvis@gmail.com>
2023-06-29 07:36:17 +0800

    deleted the comment implem

--- cypress/e2e/ui/actions/AccountScenario.js
+++ cypress/e2e/ui/actions/AccountScenario.js
@@ -11,38 +11,6 @@ const tabBar = new TabBar();
 
 export class AccountScenario {
 
-    // createNewAccount(accountInformation) {
-    //     let details = accountInformation.rowsHash();
-    //     tabBar.elements.accountTab().find('.gw-action--expand-button').click();
-    //     tabBar.elements.accountTab_NewAccount().click();
-    //     newAccountScreen.elements.newAccount_InputFirstName().type(details["firstName"]);
-    //     newAccountScreen.elements.newAccount_InputLastName().type(details["lastName"]);
-    //     newAccountScreen.elements.newAccount_searchButton().click();
-
-    //     if(newAccountScreen.elements.newAccount_NewAccountSearchResultsLV().find('tr').should('have.length.greaterThan', 1)){
-    //         newAccountScreen.elements.newAccount_NewAccountSearchResultsLV().find('div[id$=-AccountNumber_button]').invoke('text').then((value) => {
-    //             examples.accountNumber = value;
-    //         });
-    //         newAccountScreen.elements.newAccount_NewAccountSearchResultsLV().find('div[id$=-AccountNumber_button]').click();
-    //     }else{
-    //         newAccountScreen.elements.newAccount_NewAccountButton().click();
-    //         newAccountScreen.elements.newAccount_NewAccountButton_NewPerson().click();
-    //         createAccountScreen.elements.primaryEmail().type(details["email"]);
-    //         createAccountScreen.elements.address1().type(details["address1"]);
-    //         createAccountScreen.elements.city().type(details["city"]);
-    //         createAccountScreen.elements.addressType().find('select').select(details["addressType"]);
-    //         createAccountScreen.elements.selectOrganization().click();
-    //         createAccountScreen.elements.organizationName().type("Enigma");
-    //         createAccountScreen.elements.organizationSearchButton().click();
-    //         createAccountScreen.elements.organizationSearchResults().find('div[id$=-_Select]').click();
-    //         createAccountScreen.elements.producerCode().find('select').select("Standard Code Internal Producer Code - 1");
-    //         createAccountScreen.elements.update().click();
-    //         accountFileSummary.elements.accountNumber().invoke('text').then((myValue) =>{
-    //             examples.accountNumber = myValue;
-    //         });
-    //     }
-    // }
-
     createNewAccount() {
         tabBar.elements.accountTab().find('.gw-action--expand-button').click();
         tabBar.elements.accountTab_NewAccount().click();
@@ -51,11 +19,13 @@ export class AccountScenario {
         newAccountScreen.elements.newAccount_searchButton().click();
         newAccountScreen.elements.newAccount_NewAccountButton().click();
         newAccountScreen.elements.newAccount_NewAccountButton_NewPerson().click();
+        cy.wait(3000);
         createAccountScreen.elements.primaryEmail().type(world.contact.email);
         createAccountScreen.elements.address1().type(world.contact.address1);
         createAccountScreen.elements.city().type(world.contact.city);
         createAccountScreen.elements.state().find('select').select("Arizona");
         createAccountScreen.elements.zipCode().type("85001");
+        cy.wait(3000);
         createAccountScreen.elements.addressType().find('select').select("Home");
         createAccountScreen.elements.selectOrganization().click();
         createAccountScreen.elements.organizationName().find('input').type("Enigma");
@@ -65,7 +35,17 @@ export class AccountScenario {
         createAccountScreen.elements.update().click();
         accountFileSummary.elements.accountNumber().invoke('text').then((myValue) => {
             world.accountNumber = myValue;
+            cy.log(world.accountNumber);
         });
+        accountFileSummary.elements.accountHolder().invoke('text').then((accountHolder)=> {
+            const text = accountHolder.split(" ");
+            world.accountFirstname = text[0];
+            world.accountLastname = text[1];
+        });
+    }
+
+    searchByFirstAndLastName(){
+        
     }
 
     randomString(length){
