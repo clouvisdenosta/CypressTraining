c110f526a386f070a087bef23ebd0fe5d30e6e7a
clouvis <denostaclouvis@gmail.com>
2023-06-29 07:32:14 +0800

    deleted the cy.log

--- cypress/e2e/ui/actions/SearchScenario.js
+++ cypress/e2e/ui/actions/SearchScenario.js
@@ -6,22 +6,27 @@ const search = new Search();
 const accountFileSummary = new AccountFileSummary();
 export class SearchScenario {
     searchByIdentifier(searchIdentifier, identifier){
-        cy.log(identifier);
-        cy.log(searchIdentifier);
         search.elements.searchTab().click();
         if(searchIdentifier == "account"){
             if(identifier == "Account Number"){
                 search.elements.searchMenuLinksSearch_AccountSearch().click();
                 search.elements.accountSearchAccountNumber().type(world.accountNumber);
                 search.elements.accountSearchSearchButton().click();
-                cy.log(world.accountNumber);
             }
         }
-        if(searchIdentifier == "policy"){
+        else if(searchIdentifier == "policy"){
             if(identifier == "Policy Number"){
                 
             }
         }
+    }
+
+    searchByFirstAndLastName(){
+        search.elements.searchTab().click();
+        search.elements.searchMenuLinksSearch_AccountSearch().click();
+        search.elements.accountSearchFirstName().type(world.fName);
+        search.elements.accountSearchLastName().type(world.lName);
+        search.elements.accountSearchSearchButton().click();
     }
 
     verifySearchResults(identifier){
