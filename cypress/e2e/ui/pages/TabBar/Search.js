c359e852e61bd655bd5b18bee3e7f07d7368e516
clouvis <denostaclouvis@gmail.com>
2023-06-29 07:35:37 +0800

    added page element for the search page

--- cypress/e2e/ui/pages/TabBar/Search.js
+++ cypress/e2e/ui/pages/TabBar/Search.js
@@ -4,6 +4,8 @@ export class Search {
         searchTab:() => cy.get('#TabBar-SearchTab'),
         accountSearchTabMenu:() => cy.get('#TabBar-SearchTab-Search_AccountSearch'),
         accountSearchAccountNumber:() => cy.get('#AccountSearch-AccountSearchScreen-AccountSearchDV-AccountNumber'),
+        accountSearchFirstName:() => cy.get('#AccountSearch-AccountSearchScreen-AccountSearchDV-GlobalPersonNameInputSet-FirstName'),
+        accountSearchLastName: () => cy.get('#AccountSearch-AccountSearchScreen-AccountSearchDV-GlobalPersonNameInputSet-LastName'),
         accountSearchResults:() => cy.get('#AccountSearch-AccountSearchScreen-AccountSearchResultsLV'),
         searchMenuLinksSearch_AccountSearch:() => cy.get('#Search-MenuLinks-Search_AccountSearch'),
         accountSearchSearchButton:() => cy.get('#AccountSearch-AccountSearchScreen-AccountSearchDV-SearchAndResetInputSet-SearchLinksInputSet-Search')
